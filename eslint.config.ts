e12e2209aa83ce21f29a74d047b5ef3b0df2721a
Harry <ddembrs@gmail.com>
2025-06-03 15:00:58 +0800

    backup

---
+++ eslint.config.ts
@@ -0,0 +1,38 @@
+import { globalIgnores } from 'eslint/config';
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript';
+import pluginVue from 'eslint-plugin-vue';
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+  {
+    rules: {
+      'no-undef': 'off',
+      'dot-notation': 'off', // Object 不強制用 "."
+      'no-console': 'off', // 可以使用 console
+      quotes: ['error', 'single'], // 使用引號 double single
+      'semi-style': ['error', 'last'], // 强制分号出现在句子末尾。
+      'no-extra-semi': 'error', // 禁用不必要的分号。
+      semi: ['error', 'always'], // 強制使用分號
+      'no-empty-function': 'error', // 禁止空 function
+      'no-unused-labels': 'error',
+      'no-alert': 'off', // alert、confirm 和 prompt 禁止使用
+      'arrow-parens': ['error', 'always'], // ()=>箭頭
+      curly: 'off', // 可用 return 簡寫
+      'vue/no-mutating-props': 'off', // change props
+      'vue/multi-word-component-names': 'off', // 多單字的名字
+    }
+  }
+);
